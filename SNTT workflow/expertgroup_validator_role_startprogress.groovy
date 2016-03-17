46adeb89e29de5c438ac29ea2c85232f6df9f6d8
jmjdamen <jmjdamen@gmail.com>
2016-03-17 16:01:33 +0100

    new validator added

---
+++ SNTT workflow/expertgroup_validator_role_startprogress.groovy
@@ -0,0 +1,30 @@
+import com.atlassian.jira.ComponentManager
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.security.roles.ProjectRole
+import com.atlassian.jira.security.roles.ProjectRoleManager
+import com.opensymphony.workflow.InvalidInputException
+
+ComponentManager componentManager = ComponentManager.getInstance()
+def currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+//customfield 'Expert Group' , 12201 jira-test
+def cf = customFieldManager.getCustomFieldObject("customfield_12201")
+
+//if assignee is not Unassigned and Expert Group is not set
+if (issue.assignee != null && cf != null){
+
+  ProjectRoleManager projectRoleManager = ComponentManager.getComponentInstanceOfType(ProjectRoleManager.class) as ProjectRoleManager
+  ProjectRole userRole = projectRoleManager.getProjectRole((String) issue.getCustomFieldValue(cf))
+
+  if(projectRoleManager.isUserInProjectRole(currentUser, userRole, issue.projectObject)){
+    log.error ("Current user IS in Expert Group!")
+    return true
+  }
+  else{
+        invalidInputException = new InvalidInputException("Current user NOT in Expert Group!")
+        log.error ("Current user NOT in Expert Group!")
+        return false
+    }
+}
