15425bbd58c3688f76dce79964147756afdcde8d
jmjdamen <jmjdamen@gmail.com>
2016-06-28 16:57:27 +0200

    Set assignee to unassigned at postfunction create subtasks actions

--- SNWP workflow/createSubtasks_basedOnConditions_Checkboxes_postfunctionSteps.groovy
+++ SNWP workflow/createSubtasks_basedOnConditions_Checkboxes_postfunctionSteps.groovy
@@ -4,12 +4,15 @@ Screen: SNWP Change Accepted Transition Screen
 --------
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Functional Design")
 issue.summary = 'FUNCTIONAL DESIGN - ' + sourceIssue.summary
-
+issue.assignee = null
+*
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Technical Design")
 issue.summary = 'TECHNICAL DESIGN - ' + sourceIssue.summary
-
+issue.assignee = null
+*
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Category and Risk Assessment")
 issue.summary = 'CATEGORY AND RISK ASSESSMENT - ' + sourceIssue.summary
+issue.assignee = null
 
 
 Customfield: Subtasks to Create (Execution Phase)
@@ -18,27 +21,35 @@ Screen: SNWP Change In Progress Transition Screen
 --------
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Test Scenarios")
 issue.summary = 'TEST SCENARIOS - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Release and Deployment Plan")
 issue.summary = 'RELEASE AND DEPLOYMENT PLAN - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Build")
 issue.summary = 'BUILD - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Functional Documentation")
 issue.summary = 'FUNCTIONAL DOCUMENTATION - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Technical Documentation")
 issue.summary = 'TECHNICAL DOCUMENTATION - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Set up Test Environment")
 issue.summary = 'SET UP TEST ENVIRONMENT - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Execute Tests")
 issue.summary = 'EXECUTE TESTS - ' + sourceIssue.summary
+issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Implementation")
 issue.summary = 'IMPLEMENTATION - ' + sourceIssue.summary
+issue.assignee = null
 
 
 
