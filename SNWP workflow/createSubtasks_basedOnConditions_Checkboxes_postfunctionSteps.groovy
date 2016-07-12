9991c453a4ece717969819e76ca790f3d1a7b170
jmjdamen <jmjdamen@gmail.com>
2016-07-12 14:37:28 +0200

    Removed Uppercase

--- SNWP workflow/createSubtasks_basedOnConditions_Checkboxes_postfunctionSteps.groovy
+++ SNWP workflow/createSubtasks_basedOnConditions_Checkboxes_postfunctionSteps.groovy
@@ -3,15 +3,15 @@ Transition: Accept
 Screen: SNWP Change Accepted Transition Screen
 --------
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Functional Design")
-issue.summary = 'FUNCTIONAL DESIGN - ' + sourceIssue.summary
+issue.summary = 'Functional Design - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Technical Design")
-issue.summary = 'TECHNICAL DESIGN - ' + sourceIssue.summary
+issue.summary = 'Technical Design - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Classification Phase)']*.value.contains("Category and Risk Assessment")
-issue.summary = 'CATEGORY AND RISK ASSESSMENT - ' + sourceIssue.summary
+issue.summary = 'Category and Risk Assessment - ' + sourceIssue.summary
 issue.assignee = null
 
 
@@ -20,40 +20,33 @@ Transition: Start Progress
 Screen: SNWP Change In Progress Transition Screen
 --------
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Test Scenarios")
-issue.summary = 'TEST SCENARIOS - ' + sourceIssue.summary
+issue.summary = 'Test Scenarios - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Release and Deployment Plan")
-issue.summary = 'RELEASE AND DEPLOYMENT PLAN - ' + sourceIssue.summary
+issue.summary = 'Release and Deployment Plan - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Build")
-issue.summary = 'BUILD - ' + sourceIssue.summary
+issue.summary = 'Build - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Functional Documentation")
-issue.summary = 'FUNCTIONAL DOCUMENTATION - ' + sourceIssue.summary
+issue.summary = 'Functional Documentation - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Technical Documentation")
-issue.summary = 'TECHNICAL DOCUMENTATION - ' + sourceIssue.summary
+issue.summary = 'Technical Documentation - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Set up Test Environment")
-issue.summary = 'SET UP TEST ENVIRONMENT - ' + sourceIssue.summary
+issue.summary = 'Set up Test Environment - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Execute Tests")
-issue.summary = 'EXECUTE TESTS - ' + sourceIssue.summary
+issue.summary = 'Execute Tests - ' + sourceIssue.summary
 issue.assignee = null
 *
 cfValues['Subtasks to Create (Execution Phase)']*.value.contains("Implementation")
-issue.summary = 'IMPLEMENTATION - ' + sourceIssue.summary
+issue.summary = 'Implementation - ' + sourceIssue.summary
 issue.assignee = null
-
-
-
-
-
-
-
